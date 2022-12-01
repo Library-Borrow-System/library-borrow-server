986dc3ab438a7814a15b796bf2d4b38645cfbc52
김소현 <thgus345@naver.com>
2022-12-01 16:49:08 +0900

    [INIT] 초기 프로젝트 세팅

---
+++ src/main/java/com/project/library/libraryborrowsystemserver/LibraryBorrowSystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.library.libraryborrowsystemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBorrowSystemServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryBorrowSystemServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/library/libraryborrowsystemserver/LibraryBorrowSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.library.libraryborrowsystemserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBorrowSystemServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
