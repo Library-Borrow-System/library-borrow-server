badc06c73701c14e5c98080f58f504340378d3ac
김소현 <thgus345@naver.com>
2022-12-02 19:34:49 +0900

    [FIX] 폴더 리팩토링

--- src/main/java/com/project/library/LibraryBorrowSystemServerApplication.java
+++ src/main/java/com/project/library/LibraryBorrowSystemServerApplication.java
@@ -1,4 +1,4 @@
-package com.project.library.libraryborrowsystemserver;
+package com.project.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/project/library/LibraryBorrowSystemServerApplicationTests.java
+++ src/test/java/com/project/library/LibraryBorrowSystemServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.project.library.libraryborrowsystemserver;
+package com.project.library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
