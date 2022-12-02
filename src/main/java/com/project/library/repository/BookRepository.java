56320e217e3c90e3e856d7be3a6b5881f14d7857
김소현 <thgus345@naver.com>
2022-12-02 19:32:23 +0900

    [CREATE] Book 데이터 관리 Repository 생성

---
+++ src/main/java/com/project/library/repository/BookRepository.java
@@ -0,0 +1,20 @@
+package com.project.library.repository;
+
+import com.project.library.domain.Book;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BookRepository {
+
+    Book insert(Book book);
+
+    List<Book> findAll();
+
+    Optional<Book> findById(UUID bookId);
+
+    Book update(Book book);
+
+    Book deleteById(UUID bookId);
+}
