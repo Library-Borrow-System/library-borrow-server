7b76e7cb1694ebb63f42b10b155429837b05c112
김소현 <thgus345@naver.com>
2022-12-04 05:29:25 +0900

    [ADD] CORS HTTP Method 추가

--- src/main/java/com/project/library/configuration/MvcConfiguration.java
+++ src/main/java/com/project/library/configuration/MvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.project.library.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +10,16 @@ public class MvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("*");
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.OPTIONS.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.DELETE.name()
+                );
     }
 }
